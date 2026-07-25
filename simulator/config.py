277d9fed0fae31b4865ed7b372759d3d4ac8d662
Jaimin2687 <jaiminparmar2687@gmail.com>
2026-07-25 17:00:24 +0530

    Add Render deployment blueprint and update Simulator to bind to cloud PORT

--- simulator/config.py
+++ simulator/config.py
@@ -3,4 +3,4 @@ import os
 ORCHESTRATOR_URL = os.getenv('ORCHESTRATOR_URL', 'http://localhost:4000/api/telemetry')
 EVENT_RATE_MS = int(os.getenv('EVENT_RATE_MS', '800'))
 TIME_SCALE = float(os.getenv('TIME_SCALE', '1.0'))
-CONTROL_PORT = 5555
+CONTROL_PORT = int(os.getenv('PORT', '5555'))
